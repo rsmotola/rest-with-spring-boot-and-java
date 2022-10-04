c4959edeea3ba4a764eadd99902a1d26fb37bf57
Ronaldo Motola <rsmotola@gmail.com>
2022-10-04 17:08:48 -0300

    Estudando Seção 15

---
+++ rest-with-spring-boot-and-java/src/main/java/com/rsmotola/services/BookServices.java
@@ -0,0 +1,88 @@
+package com.rsmotola.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.stereotype.Service;
+
+import com.rsmotola.controllers.BookController;
+import com.rsmotola.data.vo.v1.BookVO;
+import com.rsmotola.exceptions.RequiredObjectIsNullException;
+import com.rsmotola.exceptions.ResourceNotFoundException;
+import com.rsmotola.mapper.DozerMapper;
+import com.rsmotola.model.Book;
+import com.rsmotola.repositories.BookRepository;
+
+@Service
+public class BookServices {
+	
+	private Logger logger = Logger.getLogger(BookServices.class.getName());
+	
+	@Autowired
+	BookRepository repository;
+
+	public List<BookVO> findAll() {
+
+		logger.info("Finding all book!");
+
+		var books = DozerMapper.parseListObjects(repository.findAll(), BookVO.class);
+		books
+			.stream()
+			.forEach(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
+		return books;
+	}
+
+	public BookVO findById(Long id) {
+		
+		logger.info("Finding one book!");
+		
+		var entity = repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		var vo = DozerMapper.parseObject(entity, BookVO.class);
+		vo.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+		return vo;
+	}
+	
+	public BookVO create(BookVO book) {
+
+		if (book == null) throw new RequiredObjectIsNullException();
+		
+		logger.info("Creating one book!");
+		var entity = DozerMapper.parseObject(book, Book.class);
+		var vo =  DozerMapper.parseObject(repository.save(entity), BookVO.class);
+		vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+	
+	public BookVO update(BookVO book) {
+
+		if (book == null) throw new RequiredObjectIsNullException();
+		
+		logger.info("Updating one book!");
+		
+		var entity = repository.findById(book.getKey())
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		entity.setAuthor(book.getAuthor());
+		entity.setLaunchDate(book.getLaunchDate());
+		entity.setPrice(book.getPrice());
+		entity.setTitle(book.getTitle());
+		
+		var vo =  DozerMapper.parseObject(repository.save(entity), BookVO.class);
+		vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		
+		logger.info("Deleting one book!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+	}
+
+}
---
+++ rest-with-spring-boot-and-java/src/test/java/com/rsmotola/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.rsmotola.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rsmotola.configs.TestConfigs;
+import com.rsmotola.integrationtests.testcontainers.AbstractIntegrationTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest{
+
+	@Test
+	public void shouldDisplaySwaggerUiPage() {
+		var content =
+			given()
+				.basePath("/swagger-ui/index.html")
+				.port(TestConfigs.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		assertTrue(content.contains("Swagger UI"));
+	}	
+}
