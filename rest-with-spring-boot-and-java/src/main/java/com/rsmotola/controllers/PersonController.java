9b20c65d01bc81b9be9dc54927e20c997779cd18
Ronaldo Motola <rsmotola@gmail.com>
2022-10-04 18:39:50 -0300

    Estudando Seção 16

--- rest-with-spring-boot-and-java/src/main/java/com/rsmotola/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/com/rsmotola/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+//@CrossOrigin
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
@@ -53,6 +55,7 @@ public class PersonController {
 		return service.findAll();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value = "/{id}",
 			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML} )
 	@Operation(summary = "Find a Person", description = "Find a Person",
@@ -73,24 +76,25 @@ public class PersonController {
 		return service.findById(id);
 	}
 	
+	@CrossOrigin(origins = {"http://localhost:8080", "https://erudio.com.br"})
 	@PostMapping(
-			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
-			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
-		@Operation(summary = "Adds a new Person",
-			description = "Adds a new Person by passing in a JSON, XML or YML representation of the person!",
-			tags = {"People"},
-			responses = {
-				@ApiResponse(description = "Success", responseCode = "200",
+		consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML }, 
+		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML })
+	@Operation(summary = "Adds a new Person", 
+		description = "Adds a new Person by passing in a JSON, XML or YML representation of the person!", 
+		tags = {"People" }, 
+		responses = {
+			@ApiResponse(description = "Success", responseCode = "200", 
 					content = @Content(schema = @Schema(implementation = PersonVO.class))
-				),
-				@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-				@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content), 
 			}
-		)
-		public PersonVO create(@RequestBody PersonVO person) {
-			return service.create(person);
-		}
+	)
+	public PersonVO create(@RequestBody PersonVO person) {
+		return service.create(person);
+	}
 		
 		@PutMapping(
 			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
